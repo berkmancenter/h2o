c47db38db1b7a09be3d8881ced82bdcdb2535872
Casey Gruppioni <cgruppioni@gmail.com>
2017-03-16 15:10:15 -0400

    Case search checks if case activeRecord already exists.

--- app/controllers/case_search_controller.rb
+++ app/controllers/case_search_controller.rb
@@ -11,14 +11,23 @@ class CaseSearchController < BaseController
         end
 
         response = HTTParty.get("https://capapi.org/api/v1/cases/?#{variables.to_query}&format=json")
+        @cases = response["results"]
 
-
-        @results = response["results"]
+        @cases.each do |kase|
+            if Case.where(short_name: kase["name_abbreviation"], case_jurisdiction_id: kase["jurisdiction_id"], decision_date: kase["decisiondate_original"]).exists?
+                kase["downloaded"] = true
+            else
+                kase["downloaded"] = false
+            end
+        end
 
         render :show
     end
 
     private
+
+    def preexisting?
+    end
 
     def import_case_params
         params.require(:case_search).permit(
